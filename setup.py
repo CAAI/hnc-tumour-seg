7fb3bc2f02a0ec6bcd29340002799443229563d3
teituroli <46339996+teituroli@users.noreply.github.com>
2023-05-23 09:46:05 +0200

    Update setup.py
    removing install_requires = []

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setuptools.setup(
                     "json",
                     "typing",
                     "torchio"],
-    install_requires=[],
+    #install_requires=[],
     
     entry_points={
         'console_scripts': [
